ae08b2dc3174850a0d98ca1b57e2a7853e4b294a
wangkodok <wangkodok@gmail.com>
2022-11-24 15:37:49 +0900

    리액트 event click

--- my-app/src/chapter_accordion/Accordion.jsx
+++ my-app/src/chapter_accordion/Accordion.jsx
@@ -1,26 +1,46 @@
+/* eslint-disable */
 import React from "react";
 
+function Items(props) {
+    console.log(props.text.length);
+    const arrs = []
+    for (let i = 0; i < props.text.length; i++) {
+        let items = props.text[i];
+        console.log(arrs, arrs.length);
+        console.log(arrs[0]);
+        arrs.push(
+            <li key={items.id}>
+                <a id={items.id} href="#" onClick={
+                    function(event) {
+                        event.preventDefault();
+                        props.onChangeMode(event.target.id);
+                    }
+                }>{ items.menu }</a>
+                <ul className="accordion-listSub">
+                    <li>
+                        <a href="#">{ items.content }</a>
+                    </li>
+                </ul>
+            </li>
+        )
+    }
+    return (
+        <ul className="accordion-list">
+            {arrs}
+        </ul>
+    )
+}
+
 function Accordion() {
+    const text = [
+        {id: 1, menu: 'Menu 01', content: 'Content 01'},
+        {id: 2, menu: 'Menu 02', content: 'Content 02'},
+    ]
     return (
         <div className="accordion-wrap">
-            <ul className="accordion-list">
-                <li>
-                    <a href="#">Menu 01</a>
-                    <ul className="accordion-listSub">
-                        <li>
-                            <a href="#">Content 01</a>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Menu 02</a>
-                    <ul className="accordion-listSub">
-                        <li>
-                            <a href="#">Content 02</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+            <Items text={text} onChangeMode={function(id) {
+                console.log(id);
+            }}/>
         </div>
     )
 }
