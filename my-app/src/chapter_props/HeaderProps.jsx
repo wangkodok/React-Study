ad1aa6b74875545212fc22085fd0fcfe40885805
wangkodok <wangkodok@gmail.com>
2022-12-12 17:42:46 +0900

    리액트 코드 수정

--- my-app/src/chapter_props/HeaderProps.jsx
+++ my-app/src/chapter_props/HeaderProps.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header className="header">
             <p className="header__desc">리액트 학습</p>
-            <p>입력한 글 받아서 추가하고 삭제 버튼</p>
+            <p className="header__text">입력한 글 받아서 추가하고 삭제 버튼</p>
             <span>테스트</span>
         </header>
     )
