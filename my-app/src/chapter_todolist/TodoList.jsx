27414c6e39cfc7b05d215cf8047acb0306a0f0f8
wangkodok <wangkodok@gmail.com>
2022-12-17 22:26:43 +0900

    리액트 className 추가하고 삭제하는 코드

--- my-app/src/chapter_todolist/TodoList.jsx
+++ my-app/src/chapter_todolist/TodoList.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useState } from "react";
+import "./TodoListStyle.css"
+
 
 function Todolist() {
+    const [AttributeValue, setAttributeValue] = useState(
+        [
+            'on',
+        ]
+    )
+    console.log(AttributeValue);
     return (
         <div className="todolist" style={style.todolistBorder}>
             <h1>리액트로 만들어낸 영어 단어 타자 연습</h1>
@@ -9,6 +18,17 @@ function Todolist() {
                 <input type="text" />
                 <button className="text-button">Enters</button>
             </div>
+            {/* css 클래스 추가하고 삭제하는 기능 */}
+            <button onClick={function() {
+                let descChange = [...AttributeValue];
+                console.log(descChange);
+                if (descChange[0] === 'on') {
+                    descChange[0] = 'off';
+                } else {
+                    descChange[0] = 'on';
+                }
+                setAttributeValue(descChange);
+            }} className={AttributeValue}>버튼</button>
         </div>
     )
 }
