b1ff519ca592a5d5c1b0a0e7faaa11d256c20f73
wangkodok <wangkodok@gmail.com>
2023-03-04 00:06:17 +0900

    리액트 useState( ) 사용하여 숫자 증가하는 코드

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,12 @@
+/* eslint-disable */
+
 import { useState } from 'react';
 import './App.css';
 
 function App() {
   let post = "강남 우동 맛집";
-  let [title, setTitle] = useState("글 제목");
+  let [title, setTitle] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -11,8 +14,18 @@ function App() {
         <h4 style={ {color: 'white', fontSize: '20px',} }>블로그</h4>
       </div>
       <div className="list">
-        <h4>{title}</h4>
+        <h4>{title[0]} <span onClick={() => {
+          따봉변경(따봉 + 1);
+        }}>👍</span> {따봉}</h4>
         <p>3월 3일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>3월 4일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>3월 5일 발행</p>
       </div>
     </div>
   );
