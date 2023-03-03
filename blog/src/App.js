0b004da27a93f46ecc63b46db3354ef04fa22544
wangkodok <wangkodok@gmail.com>
2023-03-03 17:58:49 +0900

    리액트 useState( ) 코드

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   let post = "강남 우동 맛집";
+  let [title, setTitle] = useState("글 제목");
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={ {color: 'white', fontSize: '20px',} }>블로그</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{title}</h4>
+        <p>3월 3일 발행</p>
+      </div>
     </div>
   );
 }
