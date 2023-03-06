b86de5b351997fbef343bb1f1f69f4eb479e4c8b
wangkodok <wangkodok@gmail.com>
2023-03-06 20:23:30 +0900

    리액트 props 응용하여 컴포넌트에게 전달한 값을 onClick 사용하여 변경하는 코드

--- blog/src/App.js
+++ blog/src/App.js
@@ -59,7 +59,7 @@ function App() {
         })
       }
       {
-        modal === true ? <Modal title={title} /> : null
+        modal === true ? <Modal title={title} setTitle={setTitle} /> : null
       }
     </div>
   );
@@ -72,6 +72,11 @@ function Modal(props) {
         <h4>{props.title[0]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+        <button onClick={() => {
+          let copy = [...props.title];
+          copy[0] = '여자 코트 추천';
+          props.setTitle(copy);
+        }}>글 수정</button>
       </div>
     </>
   )
