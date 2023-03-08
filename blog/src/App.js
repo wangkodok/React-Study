a09a8f7e58ad5b879f1eaea031e8f64be0ad99ee
wangkodok <wangkodok@gmail.com>
2023-03-08 13:07:21 +0900

    리액트 input 입력 값을 받아서 응용하기

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,7 +9,33 @@ function App() {
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
   let [modalTit, setModalTit] = useState(0);
+  let [입력값, 입력값변경] = useState("");
+  let [inputValue, setInputValue] = useState(false);
 
+  // 현재 날짜, 시간 구하기
+  let today = new Date();
+
+  // 현재 날짜
+  let year = today.getFullYear();
+  let month = ('0' + (today.getMonth() + 1)).slice(-2);
+  let day = ('0' + today.getDate()).slice(-2);
+  let currentDate = `${year}년 ${month}월 ${day}일`
+
+  // 현재 시간
+  let hours = ('0' + today.getHours()).slice(-2); 
+  let minutes = ('0' + today.getMinutes()).slice(-2);
+  let seconds = ('0' + today.getSeconds()).slice(-2); 
+  let currentTime = `${hours < 12 ? '오전' : '오후'} ${hours - 12}시 ${minutes}분 ${seconds}초`;
+
+  // useState 현재 시간
+  let [todayValueTime, setTodayValueTime] = useState([currentTime, currentTime, currentTime]);
+
+  // input 입력한 값이 없으면 실행하는 함수
+  function showTime() {
+    setTimeout(() => {
+      setInputValue(false);
+    }, 2000);
+  }
   return (
     <div className="App">
       <div className="black-nav">
@@ -56,10 +82,52 @@ function App() {
                 copy[i] = copy[i] + 1
                 따봉변경(copy);
               }}>👍</span> {따봉[i]}</h4>
-              <p>3월 4일 발행</p>
+              <p>발행일 : {currentDate} {todayValueTime[i]}</p>
+              <button onClick={() => {
+                // 해당하는 목록 삭제
+                let copy = [...title];
+                copy.splice(i, 1); // splice "i" 번째부터 1개 삭제
+                setTitle(copy);
+
+                // 해당하는 모달창 삭제
+                setModal(false);
+              }}>글 삭제</button>
             </div>
           )
         })
+      }
+      <input onChange={(e) => {
+        입력값변경(e.target.value);
+      }} />
+      <button onClick={() => {
+        // input 입력한 값이 없으면 실행하는 if 문
+        if (입력값.trim() === '') {
+          setInputValue(true);
+          showTime()
+          return;
+        }
+
+        // 입력한 값 추가
+        let copy = [...title];
+        copy.push(입력값);
+        setTitle(copy);
+
+        // 현재 시간 추가
+        let copyTime = [...todayValueTime];
+        copyTime.push(currentTime);
+        setTodayValueTime(copyTime);
+
+        // 따봉 버튼 추가
+        let 따봉카피 = [...따봉];
+        따봉카피.push(0);
+        따봉변경(따봉카피);
+      }}>글 발행</button>
+      {
+        inputValue === true ? 
+        (
+          입력값.trim() === '' ? <p>입력하지 않았습니다.</p> : null
+        )
+        : null
       }
       {
         modal === true ? <Modal title={title} setTitle={setTitle} modalTit={modalTit}/> : null
@@ -77,7 +145,7 @@ function Modal(props) {
         <p>상세내용</p>
         <button onClick={() => {
           let copy = [...props.title];
-          copy[0] = '여자 코트 추천';
+          copy[props.modalTit] = '여자 코트 추천';
           props.setTitle(copy);
         }}>글 수정</button>
       </div>
