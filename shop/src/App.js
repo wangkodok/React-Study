a5bc432566042a8c6fd8471b2a1169df4a53f0bb
wangkodok <wangkodok@gmail.com>
2023-03-08 19:13:02 +0900

    리액트 컴포넌트로 만들고 데이터 바인딩하여 map( ) 메서드 사용한 코드

--- shop/src/App.js
+++ shop/src/App.js
@@ -24,25 +24,27 @@ function App() {
       <div className="main-bg" style={{backgroundImage: 'url('+ bg +')'}}></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%" />
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%" />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%" />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].price}</p>
-          </div>
+          {
+            shoes.map((value, i) => {
+              return (
+                <Card key={i} shoes={shoes[i]} i={i} />
+              )
+            })
+          }
         </div>
       </div>
     </div>
   );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
 }
 
 export default App;
