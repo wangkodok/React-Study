39bf4420adb495caa80207286d1ffcbc9328c83c
wangkodok <wangkodok@gmail.com>
2023-03-08 17:47:47 +0900

    리액트 이미지 불러오는 코드, public 폴더에 있는 이미지 사용할 때 쓰는 코드

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,5 +1,8 @@
+/* eslint-disable */
+
 import './App.css';
 import {Navbar, Container, Nav} from 'react-bootstrap';
+import bg from './img/bg.png';
 
 function App() {
   return (
@@ -14,6 +17,32 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+      <div className="main-bg" style={{backgroundImage: 'url('+ bg +')'}}></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            {/* public 폴더에 있는 이미지 사용할 때 쓰는 코드 process.env.PUBLIC_URL + '/경로/이미지.jpg' */}
+            <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
