7f523b3852b33f5d9c64b1f9c654a7316b668acf
wangkodok <wangkodok@gmail.com>
2023-03-09 16:42:16 +0900

    리액트 라우터 설치와 세팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,12 +5,24 @@ import { useState } from 'react';
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import bg from './img/bg.png';
 import data from "./data";
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   let [shoes] = useState(data);
 
   return (
     <div className="App">
+
+      {/* URL 작성하여 페이지 이동 */}
+      <Routes>
+        <Route path="/" element={<div>메인페이지</div>}/>
+        <Route path="/detail" element={<div>서브페이지</div>}/>
+      </Routes>
+
+      {/* 클릭하여 페이지 이동 */}
+      <Link to="/">메인페이지</Link>
+      <Link to="/detail">서브페이지</Link>
+
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
