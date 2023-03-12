a2bdeeff7062a1be10d39f0d9aafed85297f345b
wangkodok <wangkodok@gmail.com>
2023-03-12 16:15:30 +0900

    refactoring

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,22 +1,6 @@
 /* eslint-disable */
 
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
-
-let BoxDiv = styled.div`
-  background-color: grey;
-  padding: 20px;
-`
-
-let YellowButton = styled.button`
-  background-color: ${props => props.bg};
-  color: ${props => props.bg === 'blue' ? 'white' : 'black'};
-  padding: 10px;
-`
-
-let PurpleButton = styled(YellowButton)`
-  background-color: purple;
-`
 
 function Detail(props) {
   let {id} = useParams();
@@ -29,11 +13,6 @@ function Detail(props) {
     찾은상품 === undefined ?
     null :
     <div className="container">
-      <BoxDiv>
-        <YellowButton bg="blue">버튼</YellowButton>
-        <YellowButton bg="orange">버튼</YellowButton>
-        <PurpleButton>버튼</PurpleButton>
-      </BoxDiv>
       <div className="row">
         <div className="col-md-6">
           <img src={`https://codingapple1.github.io/shop/shoes${찾은상품.id + 1}.jpg`}width="100%" />
