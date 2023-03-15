3864aa6bf60c25d8e4610aca9e064dc62066fafb
wangkodok <wangkodok@gmail.com>
2023-03-15 15:37:27 +0900

    리액트 더 보기 버튼 응용
    - 더 이상 상품이 없을 때 "더 이상 상품이 없습니다." 추가
    - 더 보기 버튼 클릭 시 로딩 스피너 추가
    - 기타 수정

--- shop/src/App.js
+++ shop/src/App.js
@@ -8,9 +8,12 @@ import data from "./data";
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from "./routes/Detail";
 import axios from 'axios';
+import loadingSpinner from './img/loadingSpinner.gif';
 
 function App() {
   let [shoes, setShoes] = useState(data);
+  let [buttonCount, setButtonCount] = useState(1);
+  let [is, setIs] = useState(false);
 
   return (
     <div className="App">
@@ -25,7 +28,7 @@ function App() {
         </Container>
       </Navbar>
       <Routes>
-        <Route path="/" element={<Home shoes={shoes} setShoes={setShoes} />}/>
+        <Route path="/" element={<Home shoes={shoes} setShoes={setShoes} buttonCount={buttonCount} setButtonCount={setButtonCount} is={is} setIs={setIs}/>}/>
         <Route path="/detail/:id" element={<Detail shoes={shoes}/>}/>
       </Routes>
     </div>
@@ -47,18 +50,29 @@ function Home(props) {
           }
         </div>
       </div>
-      <button onClick={() => {
-        axios.get('https://codingapple1.github.io/shop/data2.json')
-        .then((result) => {
-          console.log(result.data);
-
-          let copy = [...props.shoes, ...result.data];
-          props.setShoes(copy)
-        })
-        .catch(() => {
-          console.log('데이터 가져오기 실패');
-        })
-      }}>더 보기</button>
+      {
+        props.is === true
+        ? <img src={loadingSpinner} alt='loadingSpinner'/>
+        : null
+      }
+      {
+        props.buttonCount < (2+1) ?
+        <button onClick={() => {
+          props.setIs(true); // 데이터 로딩 중이면 로딩 스피너 추가
+          axios.get(`https://codingapple1.github.io/shop/data2.json`)
+          .then((result) => {
+            let copy = [...props.shoes, ...result.data];
+            props.setShoes(copy)
+            props.setButtonCount(props.buttonCount + 1);
+            props.setIs(false); // 데이터 로딩 끝나면 로딩 스피너 삭제
+          })
+          .catch(() => {
+            props.setIs(false); // 데이터 가져오기 실패하면 로딩 스피너 보여주지 않기
+            console.log('데이터 가져오기 실패');
+          })
+        }}>더 보기</button>
+        : <p>더 이상 상품이 없습니다.</p> 
+      }
     </>
   )
 }
@@ -67,7 +81,7 @@ function Card(props) {
   return (
     <div className="col-md-4">
       <Link to={`/detail/${props.shoes.id}`}>
-        <img src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`} width="80%" />
+        <img src={`https://codingapple1.github.io/shop/shoes${props.shoes.id + 1}.jpg`} width="80%" />
         <h4>{props.shoes.title}</h4>
         <p>{props.shoes.price}</p>
       </Link>
